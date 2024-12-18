5286582d9c513cd6e1ddfea5ab2877e39f087d30
Maren <marenehlers@gmx.net>
2024-12-18 15:24:33 +0100

    adding chat-app-content

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ChatBotStart from "./Components/ChatBotStart";
+import ChatBotApp from "./Components/ChatBotApp";
 
 const App = () => {
   return (
     <main className="container">
-      <ChatBotStart />
+      {/* <ChatBotStart /> */}
+      <ChatBotApp />
     </main>
   );
 };
