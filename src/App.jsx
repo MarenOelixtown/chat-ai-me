907361354093de3234370059096d597340d2acdf
Maren <marenehlers@gmx.net>
2024-12-19 11:00:16 +0100

    adding useState-function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import ChatBotStart from "./Components/ChatBotStart";
 import ChatBotApp from "./Components/ChatBotApp";
 
 const App = () => {
+  const [isChatting, setIsChatting] = useState(false);
+
   return (
     <main className="container">
       {/* <ChatBotStart /> */}
